2d42ff6aa128483d4d155ccf4279dd24430b31f8
close852 <close852@gmail.com>
2015-06-10 13:28:51 +0900

    meta data

--- shoppingmall/WebContent/WEB-INF/etc/fakeData.sql
+++ shoppingmall/WebContent/WEB-INF/etc/fakeData.sql
@@ -4,9 +4,9 @@ values
 ('seller-1','1111','홍길동','남','서울','df@df.com','010-1234-1234','true','false','1234-23125');
 
 insert into seller 
-(seller_id,password,name,gender,address,email,phone,auth,admin,corporate_number)
+(seller_id,password,name,gender,address,email,phone,auth,admin,corporate_number,postcode)
 values
-('admin','admin','홍길동','male','서울','df@df.com','010-1234-1234','true','true','1111-11111');
+('admin','admin','홍길동','male','서울','df@df.com','010-1234-1234','true','true','1111-11111','123-123');
 
 select  * from orders 
 
